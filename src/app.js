8d6f34efed3af3d746d7fd30da5298336db17836
aalleexxbecker <alexsandrobecker@outlook.com>
2021-09-03 15:22:17 -0300

    path file fixed

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import routers from './routers';
-import '../database';
+import routers from './app/routers';
+import './database';
 
 class App {
   constructor() {
