a43f5a220d73c90c2471349ae84a294e31121069
aalleexxbecker <alexsandrobecker@outlook.com>
2021-09-03 21:37:06 -0300

    login controller

---
+++ src/app/controllers/LoginController.js
@@ -0,0 +1,40 @@
+import User from '../models/User';
+import ApiResult from '../utils/ApiResult';
+
+class LoginController {
+  async index(req, resp) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({
+        where: { email },
+      });
+
+      if (!user) {
+        throw Error('Email inválido.');
+      }
+
+      if (!(await user.checkPassword(password))) {
+        throw Error('Senha inválida.');
+      }
+
+      const response = ApiResult.parseResult(
+        true,
+        { user },
+        'Login realizado com sucesso.'
+      );
+
+      return resp.status(ApiResult.OK_WITH_CONTENT).json(response);
+    } catch (error) {
+      const response = ApiResult.parseError(
+        false,
+        'LOGIN_ERROR',
+        error.message ? error.message : error,
+        'Não foi possível realizar o login.'
+      );
+      return resp.status(ApiResult.UNAUTHORIZED).json(response);
+    }
+  }
+}
+
+export default new LoginController();
--- src/app/routers/index.js
+++ src/app/routers/index.js
@@ -1,8 +1,9 @@
 import course from './course';
 import enrollment from './enrollment';
 import home from './home';
+import login from './login';
 import role from './role';
 import student from './student';
 import user from './user';
 
-export default [course, enrollment, home, role, student, user];
+export default [course, enrollment, home, login, role, student, user];
